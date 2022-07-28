6f55a213ed43e372b0b82ca494c9ee44d0bcb181
engrbugs <engrbugs@gmail.com>
2022-07-28 10:12:48 -0700

    done.

--- string-deconstruction.cpp
+++ string-deconstruction.cpp
@@ -1,21 +1,79 @@
 // string-deconstruction.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <filesystem>
+#include <Windows.h>
+#include "resource.h"
+#include <sstream>
+#include <iomanip>
 
 using std::cout; using std::cin;
 using std::endl; using std::string;
 
 namespace fs = std::filesystem;
+using namespace std;
+
+int put_numbers() {
+    string line;
+    cout << fs::current_path().string() + "\\clean.txt\n";
+    ifstream file_source(fs::current_path().string() + "\\clean.txt");
+    if (file_source.is_open())
+    {
+        ofstream dest(fs::current_path().string() + "\\numbered.txt");
+        if (dest.is_open())
+        {
+            int i = 0;
+            while (getline(file_source, line))
+            {
+                i++;
+                cout << line << "\n";
+                dest << std::setw(4) << std::setfill('0') << i << "-" << line << "\n";
+            }
+            file_source.close();
+            dest.close();
+        }
+        else cout << "Unable to destination file\n";
+    }
+    else cout << "Unable to open source file\n";
+
+    return 0;
+}
+
+int filter_quotes() {
+    ifstream file_source(fs::current_path().string() + "\\numbered.txt");
+    if (file_source.is_open())
+    {
+        string line;
+        ofstream file_quotes(fs::current_path().string() + "\\quotes.txt");
+        ofstream file_woquotes(fs::current_path().string() + "\\woquotes.txt");
+        while (getline(file_source, line))
+        {
+            if (line.find("”") != std::string::npos || 
+                line.find("”") != std::string::npos )
+            {
+                file_quotes << line << "\n";
+            }
+            else
+            {
+                file_woquotes << line << "\n";
+            }
+
+        }
+        file_source.close();
+        file_quotes.close();
+        file_woquotes.close();
+    }
+    return 0;
+}
 
 int main()
 {
-    cout << "Hello World!\n";
-    string vehiclesList[10] = { "value0", "value1", "value2","value3", "value4", "value5","value6", "value7","value8", "value9" };
 
-
+    cout << " Hello World!\n";
+    // 1. // put_numbers();
+    filter_quotes(); // 2.
     cout << "Current path is " << fs::current_path() << '\n'; // (1)
 
 }
