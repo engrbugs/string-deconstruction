7b087a709c85cf48305145f053ef69da03a9a533
engrbugs <engrbugs@gmail.com>
2022-08-09 23:28:52 -0700

    2022-08-09-2328

--- string-deconstruction.cpp
+++ string-deconstruction.cpp
@@ -8,6 +8,8 @@
 #include "resource.h"
 #include <sstream>
 #include <iomanip>
+#include <vector>
+#include <cstdio>
 
 #include <algorithm>
 
@@ -72,35 +74,72 @@ int filter_quotes() {
 }
 
 int organize_quotes() {
-    //string line1 = "“He has taken on the role of puppet master, ” Demeter had chided.“Deciding fates as if he were one of the Moirai himself.He should be ashamed.”";
-    //string line2 = "The girl smiled and asked, “Vanilla latte?”";
-    //string line3 = "Persephone couldn’t deny she was curious. This was the thrill she’d been seeking—the adventure she craved. “Tell me.”";
-    //string line4 = "“Excuse me?” Persephone asked.";
-    //string line5 = "Persephone couldn’t deny she was curious. This was the thrill she’d been seeking—the adventure she craved. “Tell me.”";
-    //string line6 = "“You are mistaken.”";
-    //string line7 = "3062-“You left me desperate, swollen with need only for you,” he gritted out. For a moment, she thought he might leave her desperate in return, but then he said, “But I will be a generous lover.”";
-    // 
-    //
+    string line1 = "“He has taken on the role of puppet master, ” Demeter had chided. “Deciding fates as if he were one of the Moirai himself.He should be ashamed.”";
+    string line2 = "The girl smiled and asked, “Vanilla latte?”";
+    string line3 = "Persephone couldn’t deny she was curious. This was the thrill she’d been seeking—the adventure she craved. “Tell me.”";
+    string line4 = "“Excuse me?” Persephone asked.";
+    string line5 = "Persephone couldn’t deny she was curious. This was the thrill she’d been seeking—the adventure she craved. “Tell me.”";
+    string line6 = "“You are mistaken.”";
+    string line7 = "3062-“You left me desperate, swollen with need only for you,” he gritted out. For a moment, she thought he might leave her desperate in return, but then he said, “But I will be a generous lover.”";
+    
+    string line = line1;
+    //count how many quotes the line have.
+    std::string::difference_type n = std::count(line.begin(), line.end(), '“');
 
-    //std::string s = "a_b_c";
-    //std::string::difference_type n = std::count(line1.begin(), line1.end(), '“');
+    vector<string> entry;
+    size_t start = 0;
+    for (int i = 0; i <= n - 1; i++) {
+        cout << i+1 << endl;
 
-    //cout << n << "\n";
-
-    //return 0;
-
-    ifstream file_source(fs::current_path().string() + "\\quotes.txt");
-    if (file_source.is_open())
-    {
-        string line;
-        while (getline(file_source, line))
-        {
-            std::string::difference_type n = std::count(line.begin(), line.end(), '“');
-            if (n > 2) cout << line << "\n";
-            // https://cplusplus.com/reference/string/string/find/
+        start = line.find('“', start);
+        size_t end = line.find('”', start);
+        size_t period = line.find('.', end);
+        if (start == 0) {
+            entry.push_back(line.substr(start, period + 1));
         }
+        else if (period == -1) {
+            //find the period in reverse
+            cout << -1 << endl;
+            size_t period = line.rfind('.', start);
+            size_t end_quote = line.rfind('”', start);
+            cout << period << ' ' << end_quote << endl;
+        }
+
+        //replace(entry.begin(), entry.end(), entry[entry.size() - 1], string{ "Bill" });
+
+        /*size_t end = line.find('”', start);
+
+
+        size_t prior_period = line.rfind('h', start);
+        
+        size_t period2 = line.find('.', period+1);
+        size_t len = line.length();
+
+        cout << prior_period << endl;
+        cout << ((period == -1) ? "lll" : "A") << endl;*/
+
+        //cout << loc << " " << end << period << period2 << len << endl;
+ 
+        start++;
+    }
+    for (vector<string>::iterator t = entry.begin(); t != entry.end(); ++t)
+    {
+        cout << *t << endl;
     }
     return 0;
+
+    //ifstream file_source(fs::current_path().string() + "\\quotes.txt");
+    //if (file_source.is_open())
+    //{
+    //    string line;
+    //    while (getline(file_source, line))
+    //    {
+    //        std::string::difference_type n = std::count(line.begin(), line.end(), '“');
+    //        if (n > 2) cout << line << "\n";
+    //        // https://cplusplus.com/reference/string/string/find/
+    //    }
+    //}
+    //return 0;
 }
 
 int main()
